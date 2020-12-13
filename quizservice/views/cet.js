f35eab410f9d712c1564122f5278c5944b16fc0c
rupalz <50055393+rupalz@users.noreply.github.com>
2020-12-13 14:27:49 +0530

    Update cet.js

--- quizservice/views/cet.js
+++ quizservice/views/cet.js
@@ -1,45 +1,50 @@
-var express = require('express');
-var mongoose = require('mongoose');
-const { userInfo } = require('os');
-var path = require('path');
-var bodyParser = require("body-parser");
+//This file is not doing anything
+//UPLOADED BY MISTAKE
 
 
-var indexRouter = require('./routes/index');
 
 
-mongoose.connect("mongodb+srv://dummy:qwerty@12345@cluster0.oyd5u.mongodb.net/<dbname>?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
-const connection = mongoose.connection;
-connection.once('open',() =>
-{
-    console.log("MongoDb  established successfully");
-});
+// var express = require('express');
+// var mongoose = require('mongoose');
+// const { userInfo } = require('os');
+// var path = require('path');
+// var bodyParser = require("body-parser");
 
-var Schema = new mongoose.Schema({
-    name: String,
-    });
+
+// var indexRouter = require('./routes/index');
+
+// mongoose.connect("mongodb+srv://dummy:qwerty@12345@cluster0.oyd5u.mongodb.net/<dbname>?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+// const connection = mongoose.connection;
+// connection.once('open',() =>
+// {
+//     console.log("MongoDb  established successfully");
+// });
+
+// var Schema = new mongoose.Schema({
+//     name: String,
+//     });
      
-var user = mongoose.model('emp', Schema);
+// var user = mongoose.model('emp', Schema);
 
-var app = express();
+// var app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static("public"));
 
-app.set('views', path.join(__dirname, 'views'));
- app.set('view engine', 'ejs');
-app.get("/", function(req, res){
-    user.findOne({},{},{sort : {_id: -1}},function(err, foundItems){
-    console.log(foundItems);
-    res.render("score", { name: foundItems.name});
-    })
+// app.set('views', path.join(__dirname, 'views'));
+//  app.set('view engine', 'ejs');
+// app.get("/", function(req, res){
+//     user.findOne({},{},{sort : {_id: -1}},function(err, foundItems){
+//     console.log(foundItems);
+//     res.render("score", { name: foundItems.name});
+//     })
   
-})
+// })
 
 
 
 
-app.listen(9000,function(){
-    console.log("Server connected....")
-})
-module.exports = app;
+// app.listen(9000,function(){
+//     console.log("Server connected....")
+// })
+// module.exports = app;
